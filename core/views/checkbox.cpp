582ab135e338e3c4f3ff1433a011a1a537f65390
Jeff Panici <jeff.panici@panici-software.com>
2017-10-07 16:52:12 -0600

    added checkbox view

---
+++ core/views/checkbox.cpp
@@ -0,0 +1,56 @@
+//
+// Ryu
+//
+// Copyright (C) 2017 Jeff Panici
+// All Rights Reserved.
+//
+// See the LICENSE file for details about the license covering
+// this source code file.
+//
+
+#include "checkbox.h"
+
+namespace ryu::core {
+
+    checkbox::checkbox(const std::string& name) : core::view(types::control, name) {
+    }
+
+    bool checkbox::value() const {
+        return _value;
+    }
+
+    void checkbox::value(bool flag) {
+        _value = flag;
+    }
+
+    void checkbox::on_draw(core::renderer& surface) {
+        auto bounds = client_bounds();
+
+        auto pal = *palette();
+        auto& fg = pal[fg_color()];
+
+        if (!enabled() || !focused()) {
+            fg = fg - 35;
+        }
+
+        surface.set_color(fg);
+        surface.draw_rect(bounds);
+
+        if (_value) {
+            surface.draw_line(bounds.left(), bounds.top(), bounds.right(), bounds.bottom());
+        }
+    }
+
+    bool checkbox::on_process_event(const SDL_Event* e) {
+        if (e->type == SDL_KEYDOWN) {
+            switch (e->key.keysym.sym) {
+                case SDLK_SPACE: {
+                    _value = !_value;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
---
+++ core/views/checkbox.h
@@ -0,0 +1,35 @@
+//
+// Ryu
+//
+// Copyright (C) 2017 Jeff Panici
+// All Rights Reserved.
+//
+// See the LICENSE file for details about the license covering
+// this source code file.
+//
+
+#pragma once
+
+#include <core/view.h>
+
+namespace ryu::core {
+
+    class checkbox  : public core::view {
+    public:
+        explicit checkbox(const std::string& name);
+
+        bool value() const;
+
+        void value(bool flag);
+
+    protected:
+        void on_draw(core::renderer& surface) override;
+
+        bool on_process_event(const SDL_Event* e) override;
+
+    private:
+        bool _value;
+    };
+
+};
+
