f1b696b3d7a93d60076e6aa3d446d13461751fe7
Jeff Panici <jeff.panici@panici-software.com>
2019-02-20 07:12:05 -0700

    pull in vendored libraries and basic CMakeLists

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// ----------------------------------------------------------------------------
+//
+// Ryu: The Arcade Construction Kit
+//
+// Copyright (C) 2019 Jeff Panici
+// All rights reserved.
+//
+// This software source file is licensed under the terms of MIT license.
+// For details, please read the LICENSE file.
+//
+// ----------------------------------------------------------------------------
+
+int main(int argc, char** argv) {
+    return 0;
+}
